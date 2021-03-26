03fbf5130bff14f8fcf14bc7b9eddec191e8be8c
ashishthomas22298 <athomas22298@gmail.com>
2021-03-26 17:29:35 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+const name = 'Ashish';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div>
+        <h1> Hello {name}!</h1>
+        <p>Your Lucky Number is { Math.floor(Math.random() * 10) } </p>
+    </div>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
