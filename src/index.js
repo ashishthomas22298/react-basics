4610cf249c226d6c7511ef444d4d09acee25a04c
ashishthomas22298 <athomas22298@gmail.com>
2021-03-26 17:42:51 -0400

    copyright done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const name = 'Ashish';
+const fName = 'Ashish';
+const lName = 'Thomas';
+
 ReactDOM.render(
     <div>
-        <h1> Hello {name}!</h1>
+        <h1> Hi { fName + " " + lName }! </h1>
         <p>Your Lucky Number is { Math.floor(Math.random() * 10) } </p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>Created by { fName + " " + lName } </p>
+        <p>Copyright&#169; { (new Date()).getFullYear() } </p>
     </div>,
     document.getElementById('root')
 );
