1a335891d295d8be4a2a4132a902e9c1277d112f
ashishthomas22298 <athomas22298@gmail.com>
2021-03-29 23:27:23 -0400

    deleted section component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import $ from "jquery";
 // components
 import Header from "./Header.jsx";
-import Section from "./Section.jsx";
 import Footer from "./Footer.jsx";
 // style
 import "../style/App.css";
@@ -13,30 +12,35 @@ function App(){
     return <div className="app" >
         <div id="top"></div>
         <Header />
-        <Section id="home" color = "red" ></Section>
-        <Section id="about" ></Section>
-        <Section id="services" ></Section>
-        <Section id="blog" ></Section>
-        <Section id="search" ></Section>
+        <section className="JumbotronContainer" id = "home">
+          <div className="Jumbotron">
+
+          </div>
+        </section>
+        <section id = "about"></section>
+        <section id = "services"></section>
+        <section id = "blog"></section>
+        <section id = "search"></section>
+
         <Footer />
     </div>;
 }
 
 $(window).on("load",function(){
+
     let firstSection = $("#home");
-    firstSection.height( firstSection.height() - $("div.Header").outerHeight() );
+    console.log(firstSection.height() + " " +  $("#Header").outerHeight());
+    firstSection.height( firstSection.height() - $("#Header").outerHeight() );
 
     // Sticky header
 
     let header = $("#Header");
-    console.log(header);
     
     window.onscroll = function() {stickyHeader()};
 
-    var sticky = header.outerHeight();
+    var sticky = (header.outerHeight());
     
     function stickyHeader() {
-        console.log(sticky + " " + window.pageYOffset);
       if (window.pageYOffset > sticky) {
         header.addClass("sticky");
       } else {
